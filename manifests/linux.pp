a43cd88ca5d0b80e00647cd689918cf03e7ad9e8
Nick Fosdick <nick@lark-it.com>
2019-11-30 22:18:18 -0700

    Adding linux.pp

--- manifests/linux.pp
+++ manifests/linux.pp
@@ -1,5 +1,7 @@
-class fcb_java::linux {
+class fcb_java::linux (
+  $package = 'java-1.8.0-openjdk-devel',
+){
   class { 'java':
-    package => 'java-1.7.0-openjdk-devel',
+    package => $package,
   }
 }
